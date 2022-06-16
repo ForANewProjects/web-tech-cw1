1dfcd2f744fd6f6b6bd8ce5b291189edb1084ed4
Tima <91124038+ForANewProjects@users.noreply.github.com>
2022-06-16 11:25:31 +0500

    Add files via upload

---
+++ WebTech.CW1.11424/js/script.js
@@ -0,0 +1,7 @@
+/* ==================================== typing animation ============================== */
+var typed = new Typed(".typing",{
+    strings:["","a 2 year Student of WIUT","currently studying Business information System", "a Front End Developer","a Back end Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop: true
+})
